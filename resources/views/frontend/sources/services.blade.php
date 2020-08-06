04ef838b83c8ca002694138f83eed2e2e047e3a0
Denis Bashkirov <era-digital@yandex.ru>
2020-08-06 12:36:55 +0300

    Update services.blade.php

--- resources/views/frontend/sources/services.blade.php
+++ resources/views/frontend/sources/services.blade.php
@@ -1,4 +1,25 @@
 @if(env('APP_ENV') == 'production')
+
+    <!-- Google Search Console -->
+    <meta name="google-site-verification" content="Wt1RKFI-XNhu8CWx6aG6wYYdawskByEp3gYOHxQkb10" />
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174660091-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-174660091-1');
+    </script>
+
+    <!-- Yandex Webmaster -->
+    <meta name="yandex-verification" content="2e7efb253224f5bc" />
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(66219841, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/66219841" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
     <!-- Jivosite -->
     <script src="//code-ya.jivosite.com/widget/WGe5rR2CE2" async></script>
+
 @endif
