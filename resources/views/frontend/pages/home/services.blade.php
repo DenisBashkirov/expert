5ace79eae26629c790dcb3cde4a99abfd51a26ec
Denis Bashkirov <era-digital@yandex.ru>
2020-08-06 22:53:32 +0300

    commit

--- resources/views/frontend/pages/home/services.blade.php
+++ resources/views/frontend/pages/home/services.blade.php
@@ -49,4 +49,22 @@
 
     </div>
 
+    <div class="services-dropdown xs-sm:mt-8 md-lg:mt-8 xl:mt-10 b-radius-10 overflow-hidden">
+        <div>
+            <h3 class="service-dropdown__category-headline xl:p-6 text-4xl b-bottom-grey-20 bg-white">Экспертизы</h3>
+            <div class="services-dropdown__category-items px-4 pb-8 bg-white-90">
+
+                <div class="md-xl:grid grid-cols-3 md:col-gap-4 lg:col-gap-4 xl:col-gap-4 md-xl:row-gap-3">
+
+                    @foreach($services['expertize'] as $service)
+                        @component('frontend.components.service-card.service-card', ['icon'=>$service['icon'], 'title'=>$service['name']])
+                        @endcomponent
+                    @endforeach
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </section>
