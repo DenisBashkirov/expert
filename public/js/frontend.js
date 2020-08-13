d3c794c4c8b55a11b6362cffb00cb88a55815b32
Denis Bashkirov <era-digital@yandex.ru>
2020-08-13 12:08:12 +0300

    commit

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -112,6 +112,18 @@ $(document).ready(function () {
     $dropdownButtons.on('click', function () {
         let $next = $(this).next();
         $next.slideToggle();
-    })
+    });
+
+    $('.js-nav-dropdown-show').on('mouseenter', function () {
+        let $dropdown = $(this).find('.nav__dropdown');
+        $dropdown.slideDown('200');
+    });
+
+    $('.js-nav-dropdown-show').on('mouseleave', function () {
+        let $dropdown = $(this).find('.nav__dropdown');
+        $dropdown.slideUp('200');
+    });
+
+
 
 });
--- resources/views/frontend/components/nav/nav.blade.php
+++ resources/views/frontend/components/nav/nav.blade.php
@@ -6,17 +6,48 @@
     ])
     @endcomponent
 
-    @component('frontend.components.nav.nav__item', [
-        'link'=>route('services', ['category'=>\Illuminate\Support\Str::slug('Экспертизы')]),
-        'text'=>'Экспертизы'
-    ])
-    @endcomponent
+    <span class="js-nav-dropdown-show">
+        @component('frontend.components.nav.nav__item', [
+            'link'=>route('services', ['category'=>\Illuminate\Support\Str::slug('Экспертизы')]),
+            'text'=>'Экспертизы'
+        ])
+        @endcomponent
+        <div class="nav__dropdown hidden absolute left-0 ml--4">
+            <div class="nav__dropdown-wrapper px-4 py-4 bg-radial-dark b-radius-10">
+                <div class="flex flex-row flex-nowrap sx-4">
+                <ul class="flex flex-col sy-2 list-none">
+                @for ($i = 0; $i < 15; $i++)
+                        <li><a class="hover-underline white-space-nowrap" href="#">{{ $services_list['expertize'][$i]->name }}</a></li>
+                    @endfor
+            </ul>
+            <ul class="flex flex-col sy-2 list-none">
+                @for ($i = 15; $i < count($services_list['expertize']); $i++)
+                    <li><a class="hover-underline white-space-nowrap" href="#">{{ $services_list['expertize'][$i]->name }}</a></li>
+                @endfor
+            </ul>
+            </div>
+            </div>
+        </div>
+    </span>
 
-    @component('frontend.components.nav.nav__item', [
-        'link'=>route('services', ['category'=>\Illuminate\Support\Str::slug('Оценка')]),
-        'text'=>'Оценка'
-    ])
-    @endcomponent
+    <span class="js-nav-dropdown-show">
+        @component('frontend.components.nav.nav__item', [
+            'link'=>route('services', ['category'=>\Illuminate\Support\Str::slug('Оценка')]),
+            'text'=>'Оценка'
+        ])
+        @endcomponent
+        <div class="nav__dropdown hidden absolute left-0 ml--4">
+            <div class="nav__dropdown-wrapper px-4 py-4 bg-radial-dark b-radius-10">
+                <div class="flex flex-row flex-nowrap sx-4">
+                <ul class="flex flex-col sy-2 list-none">
+                    @foreach($services_list['appraisal'] as $service)
+                        <a class="hover-underline white-space-nowrap" href="#">{{ $service->name }}</a>
+                    @endforeach
+                </ul>
+                </div>
+            </div>
+        </div>
+    </span>
 
     @component('frontend.components.nav.nav__item', [
         'link'=>route('service', ['service'=>\Illuminate\Support\Str::slug('Рецензирование')]),
